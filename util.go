5a7e22689001a0433e2837dbc54b5819fc8dbf8e
TwinProduction <twin@twinnation.org>
2020-04-17 11:24:59 -0400

    Fix #40: Verbose logs printing pointers instead of value

---
+++ util.go
@@ -0,0 +1,22 @@
+package main
+
+// p2v is the equivalent of referencing a pointer, but safely (no panic).
+// Should be used for printing purposes (i.e. fmt.Printf(...))
+func p2v(p interface{}) interface{} {
+	switch value := p.(type) {
+	case *string:
+		if value == nil {
+			return "<nil>"
+		} else {
+			return *value
+		}
+	case *int64:
+		if value == nil {
+			return "<nil>"
+		} else {
+			return *value
+		}
+	default:
+		return value
+	}
+}
